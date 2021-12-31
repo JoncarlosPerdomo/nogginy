140148538ebe7a115af5ca909dcf97884678512a
Joncarlos Perdomo <joncarlosperdomo@gmail.com>
2021-12-31 02:29:27 -0500

    Code Split Category Selection

--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import QuizQuestions from "./QuizQuestions/QuizQuestions";
+import CategorySelection from "./CategorySelection";
 // import PropTypes from "prop-types";
 
 const Quiz = () => {
@@ -20,7 +21,7 @@ const Quiz = () => {
   ) : (
     <div className="display">
       <div className="main">
-        <label htmlFor="trivia_difficulty">Choose a Category:</label>
+        <label htmlFor="trivia_difficulty">Choose a Difficulty:</label>
         <select
           onChange={difficultyHandler}
           name="trivia_difficulty"
@@ -34,37 +35,7 @@ const Quiz = () => {
 
         <label htmlFor="trivia_category">Choose a Category:</label>
 
-        <select
-          onChange={categoryHandler}
-          name="trivia_category"
-          data-testid="category"
-        >
-          <option value="any">Any Category</option>
-          <option value="9">General Knowledge</option>
-          <option value="10">Entertainment: Books</option>
-          <option value="11">Entertainment: Film</option>
-          <option value="12">Entertainment: Music</option>
-          <option value="13">Entertainment: Musicals &amp; Theatres</option>
-          <option value="14">Entertainment: Television</option>
-          <option value="15">Entertainment: Video Games</option>
-          <option value="16">Entertainment: Board Games</option>
-          <option value="17">Science &amp; Nature</option>
-          <option value="18">Science: Computers</option>
-          <option value="19">Science: Mathematics</option>
-          <option value="20">Mythology</option>
-          <option value="21">Sports</option>
-          <option value="22">Geography</option>
-          <option value="23">History</option>
-          <option value="24">Politics</option>
-          <option value="25">Art</option>
-          <option value="26">Celebrities</option>
-          <option value="27">Animals</option>
-          <option value="28">Vehicles</option>
-          <option value="29">Entertainment: Comics</option>
-          <option value="30">Science: Gadgets</option>
-          <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
-          <option value="32">Entertainment: Cartoon &amp; Animations</option>
-        </select>
+        <CategorySelection categoryHandler={categoryHandler} />
 
         <button
           type="button"
